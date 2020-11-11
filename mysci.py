f2abc60a22540dd6a06fe7d3417f4e5aea462b23
islasimpson <islas@ucar.edu>
2020-11-11 08:24:08 -0700

    Parsing file

--- mysci.py
+++ mysci.py
@@ -1,24 +1,57 @@
-print("Hello, world!")
+# Initialize my data variable
+data = []
+
+# Read the data file
 filename = "./data/wxobs20170821.txt"
 
-#datafile = open(filename,'r')
-#print(datafile.readline())
-#print(datafile.readline())
-#print(datafile.readline())
-#print(datafile.readline())
-
-#datafile = open(filename,'r')
-#data = datafile.read()
-#datafile.close()
-
-#using the with statement assures that the file is automatically
-#closed once we have finished reading it.  This will do the same as the
-# above three lines but it is more concise.
-# this translates into, with the file opened, you want to read the data 
-# and save it in data
 with open(filename,'r') as datafile:
-    data = datafile.read()
 
-# DEBUG
+    # read the first three lines (header)
+
+    for _ in range(3): # underscore is a placeholder. Can be any variable, but the underscore is typically used when you're never going to use this variable.
+    # for i in [0,1,2]: would be equivalent but range is a little cleaner.
+        print(_)
+        datafile.readline()
+
+    # Read and parse the rest of the file
+    for line in datafile: 
+        datum = line.split() #string.split() allows you to split a string at a given character.  () means you're splitting along white space.  
+                             # if you have a comma separated file you'd use datum = line.split(',')
+        data.append(datum)
+
+#DEBUG
+
+
+
+
+# This doesn't work... because data[5:8] doesn't have a 4th element.  The 4 is indexing a new list 5:8
+#print(data[5:8][4])
+
+#printing the 5th element from row 9
+#print(data[8][4])
+#
+## printing the first character of the 5th element from row 9
+#print(data[8][4][0])
+#
+##print the first 5 indices from row 9
+#print(data[8][:5])
+#
+## print every other index of row 9
+#print(data[8][::2])
+
+#print(data[0]) # just printing the first element of the list
+#print(data[9])
+#print(data[-1])
+#print(data[0:10])
+
+#for datum in data[:10]:
+#    print(datum)
+
+#for datum in data[:10:2]: # starting at zero, going to 9, with a step of 2
+#    print(datum)
 
 #print(type(data))
+
+#for datum in data:
+#    print(datum) #this goes through the list of lists and then prints each element.
+
